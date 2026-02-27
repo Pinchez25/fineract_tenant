c1aaa46d3ffa6d76db536864ac8eeb67543074ee
Pinchez25 <thuamwangi001@gmail.com>
2026-02-27 12:54:00 +0300

    Add script for creating a tenant using add_tenant.py

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "Example for Creating tenant: bank_a ..."
+
+python add_tenant.py \
+  --tenant-id my_tenant \
+  --tenant-name "My Tenant" \
+  --db-name bank_a \
+  --db-username root \
+  --db-password mysql \
+  --sys-db-username root \
+  --sys-db-password mysql \
+  --master-password fineract \
+
+echo "Tenant bank_a created successfully."
